a843b622489f68f78eb397842adc404ae3ea2fd4
adnanrahman0903 <adnanrahman8112001@gmail.com>
2023-05-12 10:19:02 +0600

    some edit

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,30 +1,30 @@
-document.getElementById('btn-withdraw').addEventListener('click', function () {
-    const withdrawAmount = document.getElementById('withdraw-amount');
-    const withdrawInputString = withdrawAmount.value;
-    const withdrawFieldValue = parseFloat(withdrawInputString);
-    withdrawAmount.value = '';
+document.getElementById("btn-withdraw").addEventListener("click", function () {
+  const withdrawAmount = document.getElementById("withdraw-amount");
+  const withdrawInputString = withdrawAmount.value;
+  const withdrawFieldValue = parseFloat(withdrawInputString);
+  withdrawAmount.value = "";
 
-    if (isNaN(withdrawFieldValue)) {
-        alert('Please enter an amount')
-        return;
-    }
+  if (isNaN(withdrawFieldValue)) {
+    alert("Please enter an amount");
+    return;
+  }
 
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const withdrawTotalString = withdrawTotal.innerText;
-    const withdrawValue = parseFloat(withdrawTotalString);
+  const withdrawTotal = document.getElementById("withdraw-total");
+  const withdrawTotalString = withdrawTotal.innerText;
+  const withdrawValue = parseFloat(withdrawTotalString);
 
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceInputString = balanceTotal.innerText;
-    const balanceValue = parseFloat(balanceInputString);
+  const balanceTotal = document.getElementById("balance-total");
+  const balanceInputString = balanceTotal.innerText;
+  const balanceValue = parseFloat(balanceInputString);
 
-    if (withdrawFieldValue > balanceValue) {
-        alert('you cannot withdraw more than your balance');
-        return;
-    }
+  if (withdrawFieldValue > balanceValue) {
+    alert("You cannot withdraw more than your balance");
+    return;
+  }
 
-    const newWithdrawAmount = withdrawFieldValue + withdrawValue;
-    withdrawTotal.innerText = newWithdrawAmount;
+  const newWithdrawAmount = withdrawFieldValue + withdrawValue;
+  withdrawTotal.innerText = newWithdrawAmount;
 
-    const newBalance = balanceValue - withdrawFieldValue;
-    balanceTotal.innerText = newBalance;
-})
+  const newBalance = balanceValue - withdrawFieldValue;
+  balanceTotal.innerText = newBalance;
+});
